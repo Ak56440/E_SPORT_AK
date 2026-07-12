a866fa7918d15c9a1f20a0b8ea48402f8787fa95
Ak56440 <akashmali56440p@gmail.com>
2026-07-12 23:45:07 +0530

    Refactor Firebase initialization and add Firestore
    Updated Firebase imports to use CDN links and added Firestore initialization.

--- firebase.js
+++ firebase.js
@@ -1,11 +1,10 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import {
+  getFirestore
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC0D2V2ILEWeJ9wRSMYMVXlMjAxYsEYY6M",
+  apiKey: "YOUR_API_KEY",
   authDomain: "ffturnaments-890b3.firebaseapp.com",
   projectId: "ffturnaments-890b3",
   storageBucket: "ffturnaments-890b3.firebasestorage.app",
@@ -13,5 +12,7 @@ const firebaseConfig = {
   appId: "1:863052947262:web:4ac0a1999f61361c16566f"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
