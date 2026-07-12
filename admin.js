c9327b1a4a83ee6278249c8a5c235f979228b2b3
Ak56440 <akashmali56440p@gmail.com>
2026-07-12 23:56:35 +0530

    Load and display team data from Firestore
    Fetch and display team registrations from Firestore.

--- admin.js
+++ admin.js
@@ -1,1 +1,30 @@
+import { db } from "./firebase.js";
 
+import {
+  collection,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const tableBody = document.getElementById("tableBody");
+
+async function loadTeams() {
+    const querySnapshot = await getDocs(collection(db, "registrations"));
+
+    tableBody.innerHTML = "";
+
+    querySnapshot.forEach((doc) => {
+        const team = doc.data();
+
+        tableBody.innerHTML += `
+        <tr>
+            <td>${team.teamName}</td>
+            <td>${team.captainName}</td>
+            <td>${team.captainUID}</td>
+            <td>${team.mobile}</td>
+            <td>${team.email}</td>
+        </tr>
+        `;
+    });
+}
+
+loadTeams();
