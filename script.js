6d0d330fbec922a4e11ae8f8ca0416513a834227
Ak56440 <akashmali56440p@gmail.com>
2026-07-12 23:46:26 +0530

    Implement registration form submission to Firestore

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
+import { db } from "./firebase.js";
 
+import {
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const form = document.getElementById("registerForm");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const teamName = document.getElementById("teamName").value;
+    const captainName = document.getElementById("captainName").value;
+    const captainUID = document.getElementById("captainUID").value;
+    const mobile = document.getElementById("mobile").value;
+    const email = document.getElementById("email").value;
+
+    try {
+        await addDoc(collection(db, "registrations"), {
+            teamName,
+            captainName,
+            captainUID,
+            mobile,
+            email,
+            createdAt: new Date()
+        });
+
+        alert("✅ Registration Successful!");
+
+        form.reset();
+
+    } catch (error) {
+        alert("❌ Error: " + error.message);
+    }
+});
