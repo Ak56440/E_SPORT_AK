5bfe2c1c9a6df252efc003ba4b6b7364f6c4e800
Ak56440 <akashmali56440p@gmail.com>
2026-07-12 23:39:40 +0530

    Initialize Firebase in firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC0D2V2ILEWeJ9wRSMYMVXlMjAxYsEYY6M",
+  authDomain: "ffturnaments-890b3.firebaseapp.com",
+  projectId: "ffturnaments-890b3",
+  storageBucket: "ffturnaments-890b3.firebasestorage.app",
+  messagingSenderId: "863052947262",
+  appId: "1:863052947262:web:4ac0a1999f61361c16566f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
