d3fd0624ef0b9af767a86dff773b6953874ab696
Ak56440 <akashmali56440p@gmail.com>
2026-07-13 09:52:28 +0530

    Implement tournament creation form submission
    Added functionality to save tournament data to Firestore on form submission.

--- admin.js
+++ admin.js
@@ -2,11 +2,40 @@ import { db } from "./firebase.js";
 
 import {
   collection,
+  addDoc,
   getDocs
 } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
+const tournamentForm = document.getElementById("tournamentForm");
 const tableBody = document.getElementById("tableBody");
 
+// Save tournament
+tournamentForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const title = document.getElementById("title").value;
+    const date = document.getElementById("date").value;
+    const entryFee = document.getElementById("entryFee").value;
+    const prizePool = document.getElementById("prizePool").value;
+
+    try {
+        await addDoc(collection(db, "tournaments"), {
+            title,
+            date,
+            entryFee,
+            prizePool
+        });
+
+        alert("Tournament Created Successfully!");
+
+        tournamentForm.reset();
+
+    } catch (error) {
+        alert(error.message);
+    }
+});
+
+// Load registered teams
 async function loadTeams() {
     const querySnapshot = await getDocs(collection(db, "registrations"));
 
