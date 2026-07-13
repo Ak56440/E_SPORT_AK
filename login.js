45bf8bf1360221567481187eccb2102e913cadc8
Ak56440 <akashmali56440p@gmail.com>
2026-07-13 09:39:46 +0530

    Update login alert message and clean imports

--- login.js
+++ login.js
@@ -1,26 +1,18 @@
 import { auth } from "./firebase.js";
-
-import {
-  signInWithEmailAndPassword
-} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
 window.login = async function () {
-
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
     try {
-
         await signInWithEmailAndPassword(auth, email, password);
 
-        alert("Login Successful!");
+        alert("Welcome to eSports Legacy Admin!");
 
         window.location.href = "admin.html";
 
     } catch (error) {
-
         alert(error.message);
-
     }
-
 }
