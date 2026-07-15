73589e69b300277ae30d4d6758841e18dbb99c3e
Ak56440 <akashmali56440p@gmail.com>
2026-07-15 14:36:17 +0530

    Implement admin login functionality

---
+++ admin-login.js
@@ -0,0 +1,30 @@
+import { auth } from "./firebase.js";
+
+import {
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    try {
+
+        await signInWithEmailAndPassword(auth, email, password);
+
+        alert("✅ Login Successful");
+
+        window.location.href = "admin.html";
+
+    } catch (error) {
+
+        alert("❌ " + error.message);
+
+    }
+
+});
