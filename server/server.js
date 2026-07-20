c62241a452065df69136e577497ceeaf9a6e7062
Ak56440 <akashmali56440p@gmail.com>
2026-07-20 10:46:16 +0530

    Add tournament routes to server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 
 import paymentRoutes from "./routes/payment.js";
+import tournamentRoutes from "./routes/tournament.js";
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/payment", paymentRoutes);
+app.use("/api/tournament", tournamentRoutes);
 
 app.get("/", (req, res) => {
     res.send("eSports Legacy API Running");
